bb1406160fd3fd20750927d36e3ef348ff38478c
Amy Williams <csamywilliams@gmail.com>
2017-12-16 10:10:20 +0000

    Add post route to index.js, changed favicon implementation

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
+const favicon = require('serve-favicon')
 const path = __dirname + '/views/pages/';
+const route = require('path')
+
+const bodyParser = require('body-parser');
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(favicon(route.join(__dirname, '/public/images', 'favicon.ico')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-//router.use(function (req,res,next) {
 router.use('/:client', (req,res,next) => {
-  console.log("/" + req.method);
   next();
 });
 
 router.get('/', (req, res) => {
   res.render('pages/index');
+});
+
+router.post('/form', function(req, res) {
+  console.log(req.body.fullname);
+  console.log(req.body.email);
+  console.log(req.body.message);
 });
 
 router.get('/:client', (req, res) => {
@@ -31,3 +42,4 @@ app.use('*', (req, res) => {
 app.listen(3000, () => {
   console.log("Live at Port 3000");
 });
+
