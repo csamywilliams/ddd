734797ecf1b9a1c285573054d396e4f4ea814291
Amy Williams <csamywilliams@gmail.com>
2017-12-16 09:13:38 +0000

    Add initial contact form layout and styling

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-var express = require("express");
-var app = express();
-var router = express.Router();
-var path = __dirname + '/views/pages/';
+const express = require("express");
+const app = express();
+const router = express.Router();
+const path = __dirname + '/views/pages/';
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-router.use(function (req,res,next) {
+//router.use(function (req,res,next) {
+router.use('/:client', (req,res,next) => {
   console.log("/" + req.method);
   next();
 });
 
-router.get("/", function(req,res){
+router.get('/', (req, res) => {
   res.render('pages/index');
 });
 
 router.get('/:client', (req, res) => {
-    var client = req.params.client;
+    let client = req.params.client;
     res.render('pages/' + client);
 });
   
 app.use("/", router);
 
-app.use("*", function(req,res){
+app.use('*', (req, res) => {
   res.sendFile(path + "404.html");
 });
 
-app.listen(3000, function(){
+app.listen(3000, () => {
   console.log("Live at Port 3000");
 });
